376b6c616dcdac0762d5184c61618662937938fd
Ahmed Doudou <doudouahmed1998@gmail.com>
2020-03-13 15:15:29 +0100

    add full project commit

---
+++ header.php
@@ -0,0 +1,164 @@
+<?php include 'head.php'; ?>
+
+<!DOCTYPE html>
+<html class="no-js" lang="zxx" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--=== Favicon ===-->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+    <title>Malysia Car</title>
+
+    <!--=== Bootstrap CSS ===-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--=== Slicknav CSS ===-->
+    <link href="assets/css/plugins/slicknav.min.css" rel="stylesheet">
+    <!--=== Magnific Popup CSS ===-->
+    <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
+    <!--=== Owl Carousel CSS ===-->
+    <link href="assets/css/plugins/owl.carousel.min.css" rel="stylesheet">
+    <!--=== Gijgo CSS ===-->
+    <link href="assets/css/plugins/gijgo.css" rel="stylesheet">
+    <!--=== FontAwesome CSS ===-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!--=== Theme Reset CSS ===-->
+    <link href="assets/css/reset.css" rel="stylesheet">
+    <!--=== Main Style CSS ===-->
+    <link href="/style.css" rel="stylesheet">
+    <!--=== Responsive CSS ===-->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+
+    
+</head>
+
+<body class="loader-active">
+
+    
+
+    <!--=======================Javascript============================-->
+    <!--=== Jquery Min Js ===-->
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <!--=== Jquery Migrate Min Js ===-->
+    <script src="assets/js/jquery-migrate.min.js"></script>
+    <!--=== Popper Min Js ===-->
+    <script src="assets/js/popper.min.js"></script>
+    <!--=== Bootstrap Min Js ===-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!--=== Gijgo Min Js ===-->
+    <script src="assets/js/plugins/gijgo.js"></script>
+    <!--=== Vegas Min Js ===-->
+    <script src="assets/js/plugins/vegas.min.js"></script>
+    <!--=== Isotope Min Js ===-->
+    <script src="assets/js/plugins/isotope.min.js"></script>
+    <!--=== Owl Caousel Min Js ===-->
+    <script src="assets/js/plugins/owl.carousel.min.js"></script>
+    <!--=== Waypoint Min Js ===-->
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+    <!--=== CounTotop Min Js ===-->
+    <script src="assets/js/plugins/counterup.min.js"></script>
+    <!--=== YtPlayer Min Js ===-->
+    <script src="assets/js/plugins/mb.YTPlayer.js"></script>
+    <!--=== Magnific Popup Min Js ===-->
+    <script src="assets/js/plugins/magnific-popup.min.js"></script>
+    <!--=== Slicknav Min Js ===-->
+    <script src="assets/js/plugins/slicknav.min.js"></script>
+
+    <!--=== Mian Js ===-->
+    <script src="assets/js/main.js"></script>
+<style type="text/css">
+        .footer {
+            left: 0;
+            position: fixed;
+            bottom: 0; 
+            text-align: center;
+            color: white;
+            width: 100%;
+        }
+    </style>
+
+ <div class="preloader">
+        <div class="preloader-spinner">
+            <div class="loader-content">
+                <img src="assets/img/preloader.gif" alt="JSOFT">
+            </div>
+        </div>
+    </div>
+    <!--== Preloader Area End ==-->
+
+    <!--== Header Area Start ==-->
+    <header id="header-area" class="fixed-top">
+        <!--== Header Top Start ==-->
+        <div id="header-top" class="d-none d-xl-block">
+            <div class="container">
+                <div class="row">
+                    <!--== Single HeaderTop Start ==-->
+                    <div class="col-lg-3 text-left">
+                        <a href="https://www.google.com/maps/place/imm+n,+217+Boulevard+Brahim+Roudani,+Casablanca+20250/@33.5750134,-7.639789,341m/data=!3m1!1e3!4m5!3m4!1s0xda7d2c430157035:0xc641b71635983304!8m2!3d33.5752762!4d-7.6392914"><i class="fa fa-map-marker"> | LOCATION</i></a> 
+                    </div>
+                    <!--== Single HeaderTop End ==-->
+
+                    <!--== Single HeaderTop Start ==-->
+                    <div class="col-lg-3 text-center">
+                        <i class="fa fa-mobile"> | +212 01234567</i> 
+                    </div>
+                    <!--== Single HeaderTop End ==-->
+                    <div class="col-lg-3 text-left">
+                        <a href="mailto:MALYSIACAR@GMAIL.COM"><i class="fa fa-envelope"> | MALYSIACAR@GMAIL.COM</i></a>  
+                    </div>
+                    <!--== Single HeaderTop Start ==-->
+                    
+                    <!--== Single HeaderTop End ==-->
+
+                    <!--== Social Icons Start ==-->
+                    <div class="col-lg-3 text-right">
+                        <div class="header-social-icons">
+                            
+                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-youtube"></i></a>
+                        </div>
+                    </div>
+                    <!--== Social Icons End ==-->
+                </div>
+            </div>
+        </div>
+        <!--== Header Top End ==-->
+
+        <!--== Header Bottom Start ==-->
+        <div id="header-bottom">
+            <div class="container">
+                <div class="row">
+                    <!--== Logo Start ==-->
+                    <div class="col-lg-4">
+                        <a href="index.php" class="logo">
+                            <img src="assets/img/logo.png" alt="JSOFT">
+                        </a>
+                    </div>
+                    <!--== Logo End ==-->
+
+                    <!--== Main Menu Start ==-->
+                    <div class="col-lg-8 d-none d-xl-block">
+                        <nav class="mainmenu alignright">
+                            <ul>
+                                <li class=""><a href="#">HOME</a></li>
+                                <li><a href="#about.php">ABOUT</a></li>
+                                <li><a href="#services.php">SERVICES</a></li>
+                                <li><a href="#contact.php"> CONTACT </a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!--== Main Menu End ==-->
+                </div>
+            </div>
+        </div>
+        <!--== Header Bottom End ==-->
+    </header>
+    <!--== Header Area End ==-->
+
+
+    <!--== LANGUAGES ==-->
+     
--- login.php
+++ login.php
@@ -1,53 +1,89 @@
-<?php
-/*
-Author: Javed Ur Rehman
-Website: https://www.allphptricks.com/
-*/
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Login</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<?php
-	require('db.php');
-	session_start();
-    // If form submitted, insert values into the database.
-    if (isset($_POST['username'])){
-		
-		$username = stripslashes($_REQUEST['username']); // removes backslashes
-		$username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
-		$password = stripslashes($_REQUEST['password']);
-		$password = mysqli_real_escape_string($con,$password);
-		
-	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username' and password='".md5($password)."'";
-		$result = mysqli_query($con,$query) or die(mysqli_error());
-		$rows = mysqli_num_rows($result);
-        if($rows==1){
-			$_SESSION['username'] = $username;
-			header("Location: index.php"); // Redirect user to index.php
-            }else{
-				echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
-				}
-    }else{
-?> 
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
-<input type="password" name="password" placeholder="Password" required />
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+<?php   
+    include 'inc/head.php';
+    include 'connection.php';
+     ?>
+<body class="loader-active">
 
-<br /><br />
-</div>
-<?php } ?>
+    <!--== Preloader Area Start ==-->
+    <div class="preloader">
+        <div class="preloader-spinner">
+            <div class="loader-content">
+                <img src="assets/img/preloader.gif" alt="JSOFT">
+            </div>
+        </div>
+    </div>
+
+    <section id="page-title-area" class="section-padding overlay">
+        <div class="container">
+            <div class="row">
+                <!-- Page Title Start -->
+                <div class="col-lg-12">
+                    <div class="section-title  text-center">
+                        <h2>Login</h2>
+                        <span class="title-line"><i class="fa fa-car"></i></span>
+                        <h1 style="color: white">MALYSIA CAR</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="lgoin-page-wrap" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-8 m-auto">
+                	<div class="login-page-content">
+                		<div class="login-form">
+                			<h3>Welcome Back!</h3>
+							<form action="" method="post">
+								<div class="username">
+									<input type="text" placeholder="Email or Username" name="user_name" required="">
+								</div>
+								<div class="password">
+									<input type="password" placeholder="Password" name="password" required="">
+								</div>
+								<div class="log-btn">
+									<button type="submit" name="submit"><i class="fa fa-sign-in" ></i> Log In</button>
+								</div>
+                                
+                                
+							</form>
+                		</div>
+                		
+                	</div>
+                </div>
+        	</div>
+        </div>
+    </section>
+    <div class="scroll-top">
+        <img src="assets/img/scroll-top.png" alt="JSOFT">
+    </div>
 
 
-</body>
-</html>
+    <?php 
+
+    if (isset($_POST["submit"])) {
+        $user_name = $_POST["user_name"];
+        $password  = $_POST["password"];
+        $query = "SELECT * FROM users WHERE user_name='$user_name' and Password='$password'";
+        $results = mysqli_query($conn, $query);
+
+        if($results == true){
+                echo "you have a count";
+                header ('Location: index.php');
+        }
+        if ($row = $results->fetch_assoc()) {
+
+                echo $row['email']. "<h1> you have a count </H1><br> ";
+            
+    
+            } else{
+
+                echo "<h1> you don't have a count </H1><br> ";
+              }
+        
+         
+    }
+        
+
+     ?>
+ 
